b145d3e9791c94553dd92d36c4c4ead69230ca3d
Angelo <avagenas14@gmail.com>
2024-03-10 12:35:16 -0400

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import useAppStore from "host/StateService";
+import { Grid } from "grid/Grid";
+
+export const Search = () => {
+  const setMicro = useAppStore((state) => state.setMicro);
+
+  useEffect(() => {
+    setMicro("search");
+  }, []);
+
+  return (
+    <div style={{ border: "1px solid purple", margin: "2px" }}>
+      <h1>Search Page App</h1>
+      <Grid />
+    </div>
+  );
+};
